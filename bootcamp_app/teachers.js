972264a7bd037877d05ed2edf3dc86d935d166ef
Allenzzp <131452abc@gmail.com>
2022-01-06 09:34:46 +0000

    pg module

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const {Pool} = require("pg");
+const pool = new Pool({
+  user: "vagrant",
+  host: "localhost",
+  password: 123,
+  database: "bootcampx"
+});
+
+const cohort_name = process.argv[2];
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher,
+cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${cohort_name}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(obj => {
+    console.log(`${obj.cohort}: ${obj.teacher}`);
+  })
+})
+.catch(err => console.error("query error", err.stack));
